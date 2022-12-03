ee25318bfb47f88c7c9e47c8366758bfedb2be5c
Eliott Nys <eliott.nys@gmail.com>
2022-12-03 19:26:50 +0100

    3_december.2

--- 3_december/__main__.py
+++ 3_december/__main__.py
@@ -2,30 +2,49 @@ import string
 def read(file_name):
     with open(file_name, "r") as file:
         contents = file.readlines()
+    for i,val in enumerate(contents):
+        contents[i] = val.replace("\n", "")
     return contents
 
-def process(input):
+def process_compartments(input):
     for i,rucksack in enumerate(input):
-        clean = rucksack.replace("\n", "")
-        input[i] = clean[:len(clean) // 2], clean[len(clean) // 2:]
+        input[i] = rucksack[:len(rucksack) // 2], rucksack[len(rucksack) // 2:]
     return input
+
+def priority(item):
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    position = dict([(x[1],x[0] + 1) for x in enumerate(alphabet)])
+    return position[item]
 
 def priorities(rucksacks):
     sum = 0
-    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    position = dict([(x[1],x[0] + 1) for x in enumerate(alphabet)])
     for rucksack in rucksacks:
         common_item = (set(rucksack[0]) & set(rucksack[1])).pop()
-        sum += position[common_item]
+        sum += priority(common_item)
+    return sum
+
+def process_groups(input):
+    groups = []
+    for i in range(0, len(input), 3):
+        groups.append([input[i], input[i+1], input[i+2]])
+    return groups
+
+def badge_priorities(groups):
+    sum = 0
+    for group in groups:
+        badge = (set(group[0]) & set(group[1]) & set(group[2])).pop()
+        sum += priority(badge)
     return sum
 
 def main():
     input = read("input.txt")
-    rucksacks = process(input)
+    rucksacks = process_compartments(input.copy())
     print(priorities(rucksacks))
+    groups = process_groups(input)
+    print(badge_priorities(groups))
     
 
 if __name__=="__main__":
     main()
 
-#answer : 7701
+#answer : 7701 & 2644
